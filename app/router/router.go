d9243fec5d818e2194e4142eab5da2a89bd7c420
Jerry <85411418@qq.com>
2023-05-11 10:28:48 +0800

    add router

--- app/router/router.go
+++ app/router/router.go
@@ -1,9 +1,11 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http/pprof"
+
 	"gopay/app/service"
 
+	"github.com/gin-gonic/gin"
 	"github.com/go-pay/gopher/ecode"
 	"github.com/go-pay/gopher/web"
 )
@@ -23,5 +25,17 @@ func StartHttpServer(s *service.Service) (g *web.GinEngine) {
 }
 
 func initRoute(g *gin.Engine) {
+	pp := g.Group("/debug/pprof")
+	{
+		pp.GET("/index", func(c *gin.Context) { pprof.Index(c.Writer, c.Request) })
+		pp.GET("/cmdline", func(c *gin.Context) { pprof.Cmdline(c.Writer, c.Request) })
+		pp.GET("/profile", func(c *gin.Context) { pprof.Profile(c.Writer, c.Request) })
+		pp.GET("/symbol", func(c *gin.Context) { pprof.Symbol(c.Writer, c.Request) })
+		pp.GET("/trace", func(c *gin.Context) { pprof.Trace(c.Writer, c.Request) })
+	}
 
+	monitor := g.Group("/gopay/v1/monitor")
+	{
+		monitor.GET("/ping", func(c *gin.Context) { web.JSON(c, "PingOK: "+c.ClientIP(), nil) })
+	}
 }
