ed66c68dd9086009e388c58fa792801157444f98
Jerry <85411418@qq.com>
2023-05-29 15:37:46 +0800

    update

--- mysql_ddl.sql
+++ mysql_ddl.sql
@@ -12,8 +12,7 @@ CREATE TABLE IF NOT EXISTS `account`
     `utime` TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
 
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uname` (`uname`),
-    KEY `ctime` (`ctime`)
+    UNIQUE KEY `uk_uname` (`uname`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT '账户表';
 
@@ -26,13 +25,12 @@ CREATE TABLE IF NOT EXISTS `company`
     `utime` TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
 
     PRIMARY KEY (`id`),
-    KEY `name` (`name`),
-    KEY `ctime` (`ctime`)
+    KEY `idx_name` (`name`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT '公司表';
 
 
-CREATE TABLE IF NOT EXISTS `payment_info`
+CREATE TABLE IF NOT EXISTS `payment_cfg`
 (
     `id`                     BIGINT        NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
     `payment_type`           TINYINT       NOT NULL DEFAULT 0 COMMENT '支付类型：0-微信，1-支付宝',
@@ -59,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `payment_info`
 CREATE TABLE IF NOT EXISTS `app`
 (
     `id`            BIGINT       NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
-    `platform`      TINYINT      NOT NULL DEFAULT 0 COMMENT '应用平台：0-微信移动应用，1-微信网站应用，2-微信公众号，3-微信小程序，4-微信第三方平台，5-支付宝网页/移动应用，6-支付宝小程序，7-支付宝生活号，8-支付宝第三方平台',
+    `platform_type` TINYINT      NOT NULL DEFAULT 0 COMMENT '应用平台：0-微信移动应用，1-微信网站应用，2-微信公众号，3-微信小程序，4-微信第三方平台，5-支付宝网页/移动应用，6-支付宝小程序，7-支付宝生活号，8-支付宝第三方平台',
     `merchant_type` TINYINT      NOT NULL DEFAULT 0 COMMENT '商户类型：0-商户，1-服务商',
     `appid`         VARCHAR(32)  NOT NULL DEFAULT '' COMMENT '应用appid',
     `return_url`    VARCHAR(256) NOT NULL DEFAULT '' COMMENT '支付return_url',
@@ -68,13 +66,13 @@ CREATE TABLE IF NOT EXISTS `app`
     `utime`         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
 
     PRIMARY KEY (`id`),
-    KEY `platform` (`platform`),
-    KEY `appid` (`appid`)
+    KEY `idx_platform_type` (`platform_type`),
+    KEY `idx_appid` (`appid`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT 'app应用表';
 
 
-CREATE TABLE IF NOT EXISTS `app_payment_info`
+CREATE TABLE IF NOT EXISTS `app_payment_cfg`
 (
     `id`              BIGINT      NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
     `app_id`          BIGINT      NOT NULL DEFAULT 0 COMMENT 'app应用表id',
@@ -84,8 +82,31 @@ CREATE TABLE IF NOT EXISTS `app_payment_info`
     `utime`           TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
 
     PRIMARY KEY (`id`),
-    KEY `app_id` (`app_id`),
-    KEY `payment_info_id` (`payment_info_id`),
-    KEY `appid` (`appid`)
+    KEY `idx_app_id` (`app_id`),
+    KEY `idx_payment_info_id` (`payment_info_id`),
+    KEY `idx_appid` (`appid`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT 'app应用支付配置关联表';
+
+
+CREATE TABLE IF NOT EXISTS payment_order
+(
+    `id`             BIGINT       NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
+    `user_id`        BIGINT       NOT NULL DEFAULT 0 COMMENT '绑定用户id',
+    `qrcode`         VARCHAR(128) NOT NULL DEFAULT '' COMMENT '支付二维码',
+    `trade_no`       VARCHAR(32)  NOT NULL DEFAULT '' COMMENT '订单号',
+    `transaction_id` VARCHAR(64)  NOT NULL DEFAULT '' COMMENT '支付交易流水号',
+    `payment_type`   TINYINT      NOT NULL DEFAULT 0 COMMENT '支付类型：0-微信，1-支付宝',
+    `pay_money`      BIGINT       NOT NULL DEFAULT 0 COMMENT '支付金额(分)',
+    `status`         TINYINT      NOT NULL DEFAULT 0 COMMENT '订单状态：0-待支付，1-支付成功，3-订单关闭',
+    `pay_time`       DATETIME     NULL COMMENT '支付时间',
+    `remark`         VARCHAR(256) NOT NULL DEFAULT '' COMMENT '备注',
+    `notify_body`    TEXT         NULL COMMENT '回调参数信息',
+    `ctime`          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `utime`          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uk_trade_no` (`trade_no`),
+    KEY `idx_transaction_id` (`transaction_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT '支付订单表';
