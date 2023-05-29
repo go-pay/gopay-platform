03aed0d13e81c025e4cfebaefe9497f7b85f6d4e
Jerry <85411418@qq.com>
2023-05-29 14:14:21 +0800

    update

--- app/router/router.go
+++ app/router/router.go
@@ -16,7 +16,7 @@ func StartHttpServer(s *service.Service) (g *web.GinEngine) {
 	srv = s
 	g = web.InitGin(s.Cfg.Http)
 	g.Gin.TrustedPlatform = "x_forwarded_for"
-	g.Gin.Use(g.CORS(), g.Logger(false))
+	g.Gin.Use(g.CORS())
 
 	ecode.Success = ecode.NewV2(0, "SUCCESS", "成功")
 	initRoute(g.Gin)
@@ -40,17 +40,37 @@ func initRoute(g *gin.Engine) {
 	}
 	v1 := g.Group("/gopay/v1")
 	{
+		// sso
+		sso := v1.Group("/sso")
+		{
+			sso.POST("/login", ssoLogin) // 登录
+		}
+		// 用户相关
 		user := v1.Group("/user")
 		{
-			user.POST("/getInfo", userGetInfo)
+			user.POST("/getInfo", userGetInfo) // 获取用户信息
 		}
+		// 支付相关
 		pay := v1.Group("/payment")
 		{
-			pay.POST("/alipay")
+			manage := pay.Group("/manage")
+			{
+				manage.POST("/getPaymentInfoList") // 获取支付信息列表
+				manage.POST("/addPaymentInfo")     // 添加支付配置信息
+			}
+			ali := pay.POST("/alipay")
+			{
+				ali.POST("/getPaymentQrcode") // 获取支付宝支付二维码
+			}
+			wx := pay.POST("/wechat")
+			{
+				wx.POST("/getPaymentQrcode") // 获取微信支付二维码
+			}
 		}
+		// 订单相关
 		order := v1.Group("/order")
 		{
-			order.POST("/create")
+			order.POST("/getOrderList") // 获取支付订单列表
 		}
 	}
 }
